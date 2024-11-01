c2fdca18673592b83181eac0cc312fb15e70f736
hamidihabib <hamidihabib@outlook.com>
2024-11-01 13:48:04 +0100

    update

--- app/components/Location.tsx
+++ app/components/Location.tsx
@@ -1,21 +1,13 @@
+"use client"
+
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 
-interface Mensa {
-  id: number;
-  name: string;
-}
 
-const Mensen: React.FC = () => {
-  const [mensen, setMensen] = useState<Mensa[]>([]);
+const Location: React.FC = () => {
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
 
-  const fetchApiData = async (latitude: number, longitude: number) => {
-    const res = await fetch(`https://openmensa.org/api/v2/canteens?near[lat]=${latitude}&near[lng]=${longitude}&near[dist]=50000`);
-    const data = await res.json();
-    setMensen(data);
-  };
+
 
   useEffect(() => {
     if ('geolocation' in navigator) {
@@ -31,12 +23,6 @@ const Mensen: React.FC = () => {
     }
   }, []);
 
-  useEffect(() => {
-    if (latitude !== null && longitude !== null) {
-      fetchApiData(latitude, longitude);
-    }
-  }, [latitude, longitude]);
-
   return (
     <div>
       Latitude: {latitude}
@@ -45,4 +31,4 @@ const Mensen: React.FC = () => {
   );
 };
 
-export default Mensen;
+export default Location;
